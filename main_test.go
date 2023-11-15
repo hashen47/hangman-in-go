11526e74ecc16e303873cc5a7df5902afb7bee07
dilush hashen <dilushhashen11@gmail.com>
2023-11-16 00:27:43 +0530

    write testcases for every method

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"testing"
 )
@@ -45,6 +46,93 @@ func TestGetUserInput(t *testing.T) {
         case (char == "" && err != nil) || (char != "" && err == nil):
         default:
             t.Error(err)
+        }
+    }
+}
+
+
+func TestIsGameOver(t *testing.T) {
+    testValues := []struct{
+        word string
+        wordHint string
+        remain int
+        want bool
+    }{
+        {"word", "word", 0, true},
+        {"word", "word", 5, true},
+        {"dilush", "hashen", 5, false},
+        {"dilush", "hashen", 0, true},
+    }
+
+    for _, v := range testValues {
+        if current := isGameOver(&v.word, &v.wordHint, v.remain); current != v.want {
+            t.Errorf("word: %v, wordHint: %v, remain: %v, expect: %v, current: %v", v.word, v.wordHint, v.remain, v.want, current)
+        }
+    }
+}
+
+func TestPlayAgainOrExit(t *testing.T) {
+    testInputs := []struct{
+        input string
+        want bool
+    }{
+        {"s", false},
+        {"", false},
+        {"2323", false},
+        {"n", false},
+        {"y", true},
+        {"Y", true},
+    }
+
+    for _, testCase := range testInputs {
+        var isPlay = true
+        r, w, err := os.Pipe()
+
+        if err != nil {
+            t.Error(err)
+        }
+
+        if _, err := w.Write([]byte(testCase.input)); err != nil {
+            t.Error(err)
+        }
+        w.Close()
+
+        defer func (v *os.File) { os.Stdin = v } (os.Stdin)
+
+        os.Stdin = r
+
+        playAgainOrExit(&isPlay)
+
+        switch {
+        case testCase.want == isPlay:
+        default:
+            t.Errorf("input: %v, want: %v, value: %v", testCase.input, testCase.want, isPlay)
+        }
+    }
+}
+
+
+func TestIsUserGuessCorrect(t *testing.T) {
+    testCases := []struct{
+        word string
+        wordHint string
+        guess string
+        want bool
+        err error
+    }{
+        {"dilush", "______", "d", true, nil},
+        {"dilush", "d_____", "d", true, errors.New("err")},
+        {"dilush", "______", "g", false, nil},
+        {"dilush", "_i____", "i", true, errors.New("err")},
+        {"dilush", "_i____", "k", false, nil},
+    }
+
+    for _, v := range testCases {
+        if b, err := isUserGuessCorrect(&v.word, &v.wordHint, &v.guess); 
+            (b != v.want) || 
+            (err == nil && v.err != nil) || 
+            (err != nil && v.err == nil) {
+            t.Errorf("word: %v, wordHint: %v, guess: %v, want_err: %v, want_bool: %v, current_err: %v, current_bool: %v", v.word, v.wordHint, v.guess, v.err, v.want, err, b)
         }
     }
 }
