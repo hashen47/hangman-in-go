d08700efe7732d5983439018e23021150ca8a69f
dilush hashen <dilushhashen11@gmail.com>
2023-11-15 20:54:54 +0530

    add testcase to getUserInput function

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
-
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 
 func TestSelectRandomWord(t *testing.T) {
@@ -14,5 +16,35 @@ func TestSelectRandomWord(t *testing.T) {
         t.Errorf("no error also no word, word: %v, err: %v", word, err)
     case len(word) < 6:
         t.Errorf("word length is less than 6, word: %v, err: %v", word, err)
+    }
+}
+
+
+func TestGetUserInput(t *testing.T) {
+    testInputs := []string{"d", "s", "dilush", "\n", "", " ", "23", "2323"}
+
+    for _, word := range testInputs {
+        r, w, err := os.Pipe()
+
+        if err != nil {
+            t.Error(err)
+        }
+
+        if _, err := w.Write([]byte(word)); err != nil {
+            t.Error(err)
+        }
+        w.Close()
+
+        defer func (v *os.File) { os.Stdin = v } (os.Stdin)
+
+        os.Stdin = r
+
+        char, err := getUserInput()
+
+        switch {
+        case (char == "" && err != nil) || (char != "" && err == nil):
+        default:
+            t.Error(err)
+        }
     }
 }
